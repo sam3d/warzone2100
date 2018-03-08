5d43a9f1325e7ebc2574493274a69d1a8af57a6f
Sam Holmes <samholmes1337@gmail.com>
2018-03-08 11:21:21 +0000

    Add additional clone functionality

--- src/keymap.cpp
+++ src/keymap.cpp
@@ -236,6 +236,7 @@ static KeyMapSaveEntry const keyMapSaveTable[] =
 	{kf_Cheat_FinishResearch, "Cheat_FinishResearch"},
 	{kf_Cheat_Destroy, "Cheat_Destroy"},
 	{kf_Cheat_Clone, "Cheat_Clone"},
+	{kf_Cheat_Clone_More, "Cheat_Clone_More"},
 	{kf_Cheat_God, "Cheat_God"},
 	{kf_Cheat_FinishBuildings, "Cheat_FinishBuildings"}
 };
@@ -469,6 +470,7 @@ void keyInitMappings(bool bForceDefaults)
 	keyAddMapping(KEYMAP___HIDE, KEY_LALT, KEY_R, KEYMAP_PRESSED, kf_Cheat_FinishResearch, N_("Finish research"));
 	keyAddMapping(KEYMAP___HIDE, KEY_LALT, KEY_D, KEYMAP_PRESSED, kf_Cheat_Destroy, N_("Destroy"));
 	keyAddMapping(KEYMAP___HIDE, KEY_LALT, KEY_C, KEYMAP_PRESSED, kf_Cheat_Clone, N_("Clone selected units"));
+	keyAddMapping(KEYMAP___HIDE, KEY_LALT, KEY_V, KEYMAP_PRESSED, kf_Cheat_Clone_More, N_("Clone selected units more"));
 	keyAddMapping(KEYMAP___HIDE, KEY_LALT, KEY_G, KEYMAP_PRESSED, kf_Cheat_God, N_("Toggle god mode"));
 	keyAddMapping(KEYMAP___HIDE, KEY_LALT, KEY_B, KEYMAP_PRESSED, kf_Cheat_FinishBuildings, N_("Finish construction projects"));
 
